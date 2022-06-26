fc81696c3d838a7f779fde666233128d3f12147d
nomgindia <nomgindia@gmail.com>
2022-06-26 12:20:10 +0530

    initializing empty repo

---
+++ src/main/java/com/nomg/nomg_backenddev/NoMgBackendDevApplication.java
@@ -0,0 +1,13 @@
+package com.nomg.nomg_backenddev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoMgBackendDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoMgBackendDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nomg/nomg_backenddev/NoMgBackendDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomg.nomg_backenddev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoMgBackendDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
